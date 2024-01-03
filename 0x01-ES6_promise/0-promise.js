086e0fb51fdf81d74f4f26e0f02a52a03455bc47
WinnyKay3 <winffredkalondu@gmail.com>
2024-01-04 00:03:46 +0300

    .\0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,16 @@
 function getResponseFromAPI() {
-    return new Promise((resolve) => {
-      // Simulate an asynchronous API call
-      setTimeout(() => {
-        // Assuming the asynchronous operation is completed
-        const responseData = {
-          status: 200,
-          body: 'photo-profile-1',
-        };
-  
-        resolve(responseData);
-      }, 1000);
-    });
-  }
-  
-  export default getResponseFromAPI;
+  return new Promise((resolve) => {
+    // Simulate an asynchronous API call
+    setTimeout(() => {
+      // Assuming the asynchronous operation is completed
+      const responseData = {
+        status: 200,
+        body: 'photo-profile-1',
+      };
+
+      resolve(responseData);
+    }, 1000);
+  });
+}
+
+export default getResponseFromAPI;
